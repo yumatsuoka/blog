cb7930d8bbc0e43a3ebc3527bfc5bc41d425b2ad
yumatsuoka <yuma@yuma-mbp.local>
2018-01-30 17:35:01 -0500

    change _config and rebuild

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "First Post",
         "excerpt":"ブログのモチベーション情報系の学生なのにWeb系の知識がないなーと思ったので、JekyllとGithubを使ってWebページを作ってみました。このブログをちょこちょこいじってWeb系の知識を学んでいきたいと思います。書く(かもしれない)こと  日々のできごと  留学中のボルチモアの話  Johns Hopkins Univerisity(ジョンズホプキンス大学)のこと  Web系の知識で学んだことのまとめ  クライミング のこと三日坊主にならないように頑張ります。","categories": [],
         "tags": [],
-        "url": "https://yumatsuoka.github-name.github.io/blog/first-post/",
+        "url": "https://yumatsuoka.github.io/blog/first-post/",
         "teaser":null},{
         "title": "友達のねこの写真をまとめてみた",
         "excerpt":"ボルチモアで一番仲の良い友達の家にいるねこが好きすぎるアメリカのボルチモアに住み始めてはや4ヶ月。ジョンズホプキンス大学の大学院生として、研究に集中できる環境をもらっており平日はアジア人らしく、夜遅くまでラボに残っている。一方で週末はbarに飲みに行ったり、友達の家でchillingしている。その友達(自分より１０歳年上のworker)の家のねこが好きすぎてひたすら可愛がっている。餌をねだる姿が可愛すぎて、さんかく(三角形のねこの固形餌)をついつい与えすぎ、ダイエット中の彼女をつい太らせてしまう。ねこの名前は、pepper。さっそくpepperの写真を貼っていきたいがJekyllでどう画像を表示して行くのだろうか….pepperの写真を貼っていくhoge","categories": [],
         "tags": [],
-        "url": "https://yumatsuoka.github-name.github.io/blog/%E5%8F%8B%E9%81%94%E3%81%AE%E3%81%AD%E3%81%93%E3%81%AE%E5%86%99%E7%9C%9F%E3%82%92%E3%81%BE%E3%81%A8%E3%82%81%E3%81%A6%E3%81%BF%E3%81%9F/",
+        "url": "https://yumatsuoka.github.io/blog/%E5%8F%8B%E9%81%94%E3%81%AE%E3%81%AD%E3%81%93%E3%81%AE%E5%86%99%E7%9C%9F%E3%82%92%E3%81%BE%E3%81%A8%E3%82%81%E3%81%A6%E3%81%BF%E3%81%9F/",
         "teaser":null}]
