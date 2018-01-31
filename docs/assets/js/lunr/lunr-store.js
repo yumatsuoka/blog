b9dd86733078cbed1730a9b647003adb065a4248
yumatsuoka <yuma@yuma-mbp.local>
2018-01-31 17:51:12 -0500

    check image view

--- docs/assets/js/lunr/lunr-store.js
+++ docs/assets/js/lunr/lunr-store.js
@@ -1,1 +1,11 @@
-var store = []
+var store = [{
+        "title": "First post. 書きたいこと。",
+        "excerpt":"ブログのモチベーション情報系の学生なのにWeb系の知識がないなーと思ったので、JekyllとGithubを使ってWebページを作ってみました。このブログをちょこちょこいじってWeb系の知識を学んでいきたいと思います。書く(かもしれない)こと  日々のできごと  留学中のボルチモアの話  Johns Hopkins Univerisity(ジョンズホプキンス大学)のこと  Web系の知識で学んだことのまとめ  クライミング のこと三日坊主にならないように頑張ります。","categories": [],
+        "tags": ["firstpost","モチベーション"],
+        "url": "https://yumatsuoka.github.io/blog/first-post/",
+        "teaser":null},{
+        "title": "Pepper",
+        "excerpt":"title: “友達の猫の写真をまとめてみた。”tags: [猫, pepper, ブログ]—ボルチモアで一番仲の良い友達の家にいるねこが好きすぎるアメリカのボルチモアに住み始めてはや4ヶ月。ジョンズホプキンス大学の大学院生として、研究に集中できる環境をもらっており平日はアジア人らしく、夜遅くまでラボに残っている。一方で週末はbarに飲みに行ったり、友達の家でchillingしている。その友達(自分より１０歳年上のworker)の家のねこが好きすぎてひたすら可愛がっている。餌をねだる姿が可愛すぎて、さんかく(三角形のねこの固形餌)をついつい与えすぎ、ダイエット中の彼女をつい太らせてしまう。ねこの名前は、pepper。さっそくpepperの写真を貼っていきたいがどう画像を表示して行くのだろうか….![pepper01](画像のPATH)or# 出力される画像のサイズを指定。&lt;img src=\"画像のPATH\" width=\"xxxpx\"&gt;# こっちはGIF画像もいける...？今回はローカルでJekyllにより、htmlをコンパイルしてコンパイルずみのhtmlをwebページとして公開するので、PATHはこの記事を書いているmarkdownのファイルからみた画像のディレクトリを相対パスで記していく。pepperの写真を貼っていく上のmarkdownの記法のうち、下の文法を使って画像を貼ってみる。test01test02test03ここからGIF画像を２枚はる。見れるかな？","categories": [],
+        "tags": [],
+        "url": "https://yumatsuoka.github.io/blog/pepper/",
+        "teaser":null}]
